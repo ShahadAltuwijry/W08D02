61097c55484a01ba748a99b2e1f6f8e83f2bd8a5
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-11-29 19:40:48 +0300

    completed user route file

--- routers/route/user.js
+++ routers/route/user.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const userRouter = express.Router();
+const { registration, login }
+
+userRouter.post("/reg", registration );
+userRouter.post("/login", login );
 
 module.exports = userRouter;
